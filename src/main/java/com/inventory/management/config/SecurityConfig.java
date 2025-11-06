99a3b17531b052a4506e63b018dca997cd9dba0a
Binath Perera <binathpr@gmail.com>
2025-11-07 00:32:01 +0530

    Resolve cors issue

--- src/main/java/com/inventory/management/config/SecurityConfig.java
+++ src/main/java/com/inventory/management/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
 
         // 🔒 Use explicit origin — "*" will fail if you use credentials or auth headers
         // configuration.setAllowedOrigins(List.of(clientUrl));
-        configuration.setAllowedOrigins(List.of("43.204.141.135", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://43.204.141.135", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
